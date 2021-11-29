c71e92f68777e43f869aa3c293d5387d13f73227
Andre B <andrei.b10011@gmail.com>
2021-11-29 22:34:42 +0200

    Contact Requests

---
+++ app/Http/Controllers/ContactRequestsController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ContactRequests;
+
+class ContactRequestsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $contacts = ContactRequests::all();
+
+        if ($request->has('filterBy')) {
+
+            $jj = $request->get('filterBy');
+            $fields = json_decode($jj, true);
+            
+            if (array_key_exists('email', $fields)) {
+                $contacts = $contacts->where('email', $fields['email'])->first();
+            }
+
+            if (array_key_exists('is_resolved', $fields)) {
+                $contacts = $contacts->where('is_resolved', $fields['is_resolved'])->first();
+            }
+            
+            if (array_key_exists('message', $fields)) {
+                $contacts = $contacts->where('message', $fields['message'])->first();
+            }
+
+
+            return $contacts;
+        }
+
+        if ($request->has('sortBy') && $request->has('order')) {
+            if (preg_match('(email|id|name|message)', $request->get('sortBy')) === 1
+                && preg_match('(desc|asc)', $request->get('order')) === 1) {
+                return ContactRequests::orderBy($request->get('sortBy'), $request->get('order'))->get();
+            }
+        }
+        return ContactRequests::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:50'],
+            'email' =>['required', 'max:50'],
+            'message' => ['required', 'max:5000'],
+            'is_resolved' => 'sometimes|nullable'
+        ]);
+
+        $result = filter_var($request->get('email'), FILTER_VALIDATE_EMAIL);
+        if ($result == false)
+            return response()->json(['errors' => ['email' => ['The email is invalid.']]], 400);
+
+        return ContactRequests::create($request->all());
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        if (ContactRequests::select('id')->where('id',$id)->exists())
+            return ContactRequests::find($id);
+        return response()->json(['errors' => ['id' => ['The id is invalid.']]], 404);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if ($request->get('is_resolved') == 'true' or $request->get('is_resolved') == 'false') {
+            if (ContactRequests::select('id')->where('id',$id)->exists())
+                return ContactRequests::where('id', $id)->update(array('is_resolved' => $request->get('is_resolved')));
+            return response()->json(['errors' => ['Error occured.']], 404);
+        }
+        return response()->json(['errors' => ['input' => ['The input is invalid.']]], 400);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if (ContactRequests::select('id')->where('id',$id)->exists())
+            return ContactRequests::destroy($id);
+        return response()->json(['errors' => ['id' => ['The id is invalid.']]], 404);
+    }
+}
